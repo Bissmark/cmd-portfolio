1270fec0ec79f7fe0232efc58a704f2681753dfe
Christopher Holt <holt.christopher1@gmail.com>
2024-08-23 18:17:28 +1000

    set up commands for --help and other commands, they show their values in the cmd window

--- src/components/Cmd.jsx
+++ src/components/Cmd.jsx
@@ -1,9 +1,11 @@
 import { useEffect, useRef, useState } from 'react';
 import './Cmd.css';
+import { commands } from '../utilities/commands';
 
 const Cmd = () => {
     const [inputValue, setInputValue] = useState('');
     const [cmdHistory, setCmdHistory] = useState([]);
+    const [historyIndex, setHistoryIndex] = useState(-1);
     const inputRef = useRef(null);
 
     useEffect(() => {
@@ -24,8 +26,13 @@ const Cmd = () => {
 
     const _handleKeyPress = (e) => {
         if (e.key === 'Enter') {
+            if (commands[inputValue]) {
+                console.log(commands[inputValue]);
+            }
+
             setCmdHistory([...cmdHistory, inputValue]);
             setInputValue('');
+            setHistoryIndex(-1);
         }
     };
 
@@ -34,6 +41,35 @@ const Cmd = () => {
         if (e.ctrlKey && e.key === 'l') {
             e.preventDefault();
             setCmdHistory([]);
+        }
+
+        // Clear the command prompt input field
+        if (e.ctrlKey && e.key === 'c') {
+            e.preventDefault();
+            setInputValue('');
+        }
+
+        // Press the up arrow to show the previous command
+        if (e.key === 'ArrowUp') {
+            if (cmdHistory.length > 0) {
+                const newIndex = historyIndex === -1 ? cmdHistory.length - 1 : Math.max(historyIndex - 1, 0);
+                setInputValue(cmdHistory[newIndex]);
+                setHistoryIndex(newIndex);
+            }
+        }
+
+        // Press the down arrow to show the next command
+        if (e.key === 'ArrowDown') {
+            if (cmdHistory.length > 0) {
+                const newIndex = historyIndex + 1;
+                if (newIndex >= cmdHistory.length) {
+                    setInputValue('');
+                    setHistoryIndex(-1);
+                } else {
+                    setInputValue(cmdHistory[newIndex]);
+                    setHistoryIndex(newIndex);
+                }
+            }
         }
     }
 
---
+++ src/utilities/commands.js
@@ -0,0 +1,5 @@
+// This file will be for commands that you can input into the cmd window to show different things
+
+export const commands = {
+   "--help": "Here are the available commands: --help, --about, --contact, --resume\n\n If you want to clear the command prompt history, press Ctrl + L\n\n If you want to clear the command prompt input field, press Ctrl + C",
+}
